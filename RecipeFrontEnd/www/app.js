b4bde48055df008cc8c702e698f4914bcdf1bb23
Pranav Dhingra <pranavdhingra2019@u.northwestern.edu>
2017-02-10 22:21:30 +0000

    set up basic backend. Still working on linking database.

--- RecipeFrontEnd/www/app.js
+++ RecipeFrontEnd/www/app.js
@@ -14,25 +14,27 @@ var config = {
 var client  = new pg.Client()
 
 client.connect(function (err) {
-  if (err) throw err;
+  if (err) {throw err};
 });
 
 function search(foodName) {
   // Select from column 'ingredients' from table 'recipes' where
   //var q = 'WHERE ' + foodName + 'IN (SELECT ingredients FROM recipes)';
   // Select all From table 'recipes' WHERE column 'ingredients' are LIKE foodName (%foodName% means you can find string within words)
-  var alteratequery = 'SELECT * FROM recipes WHERE ingredients LIKE ' + '%' + foodName '%';
+  var alteratequery = 'SELECT * FROM recipes WHERE ingredients LIKE ' + '%' + foodName + '%';
   
    // execute a query on our database 
   client.query(alternatequery, ['brianc'], function (err, result) {
-    if (err) throw err;
+    if (err) {throw err};
  
     // just print the result to the console 
     console.log(result.rows[0]); // outputs: { name: 'brianc' } 
-}
+});
               
                
 // disconnect the client 
 client.end(function (err) {
-      if (err) throw err;
+      if (err) {throw err};
+});
+
 }
