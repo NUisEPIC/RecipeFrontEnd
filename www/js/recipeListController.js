524aad21b7e121600a201a5a2a0d288248ce9eec
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-02-18 13:19:37 -0600

    basic Recipe List implementation

--- www/js/recipeListController.js
+++ www/js/recipeListController.js
@@ -9,12 +9,26 @@
             id: '1',
             name: 'Scrambled Eggs',
             description: 'They are eggs.',
-            ingredients: ['milk', 'eggs', 'cheese']
+            ingredients: ['milk', 'eggs', 'cheese'],
+            image: 'scrambled_eggs.jpeg'
         }, {
             id: '0',
             name: 'Toast',
             description: 'Can\'t fuck this one up',
-            ingredients: ['bread']
+            ingredients: ['bread'],
+            image: 'toast.jpeg'
+        }, {
+            id: '2',
+            name: 'Scrambled Eggs',
+            description: 'They are eggs.',
+            ingredients: ['milk', 'eggs', 'cheese'],
+            image: 'scrambled_eggs.jpeg'
+        }, {
+            id: '3',
+            name: 'Toast',
+            description: 'Can\'t fuck this one up',
+            ingredients: ['bread'],
+            image: 'toast.jpeg'
         }];
 
         $(function() {
