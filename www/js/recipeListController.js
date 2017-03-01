289f0f0280c689dcae43b4a428393dc7f37d760b
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-02-28 21:54:55 -0600

    Retrieve data from recipe-service for list view

--- www/js/recipeListController.js
+++ www/js/recipeListController.js
@@ -5,33 +5,23 @@
 
     function RecipeListController() {
         var vm = this;
-        vm.recipes = [{
-            id: '1',
-            name: 'Scrambled Eggs',
-            description: 'They are eggs.',
-            ingredients: ['milk', 'eggs', 'cheese'],
-            image: 'scrambled_eggs.jpeg'
-        }, {
-            id: '0',
-            name: 'Toast',
-            description: 'Can\'t fuck this one up',
-            ingredients: ['bread'],
-            image: 'toast.jpeg'
-        }, {
-            id: '2',
-            name: 'Scrambled Eggs',
-            description: 'They are eggs.',
-            ingredients: ['milk', 'eggs', 'cheese'],
-            image: 'scrambled_eggs.jpeg'
-        }, {
-            id: '3',
-            name: 'Toast',
-            description: 'Can\'t fuck this one up',
-            ingredients: ['bread'],
-            image: 'toast.jpeg'
-        }];
+        //vm.recipes = [{
+            //id: '1',
+            //name: 'Scrambled Eggs',
+            //description: 'They are eggs.',
+            //ingredients: ['milk', 'eggs', 'cheese'],
+            //image: 'scrambled_eggs.jpeg'
+        //}
 
         $(function() {
+            $.get('https://nuisepic.com/recipedb')
+                .done(function(res) {
+                    vm.recipes = res.data;
+                })
+                .fail(function(err) {
+                    console.log(err)
+                });
+
             $('#search').keypress(function(e) {
                 // Enter key pressed
                 if (e.which == 10 || e.which == 13) { 
@@ -41,6 +31,6 @@
                     });
                 }
             });
-        })
+        });
     }
 }());
