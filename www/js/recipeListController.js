0c535dcfa1513ba1a99799947637dcb56d716b15
Pranav Dhingra <pranavdhingra2019@u.northwestern.edu>
2017-03-11 22:09:23 +0000

    wrote ajax request for search bar, but need to do some data validation and other stuff. Ethan is working on adding the checkboxes and drop downs for filtering by mealcategory, allergens and/or diet.

--- www/js/recipeListController.js
+++ www/js/recipeListController.js
@@ -27,7 +27,17 @@
                 if (e.which == 10 || e.which == 13) { 
                     $('form').submit(function() {
                         e.preventDefault();
-                        
+                        $.get('https://localhost:2345/search/', {data: $('#search').val(),
+							mealtype: $("#mealtype").val(),
+							diet: $('#diet').val(),
+							allergies: $('#allergies').val()}) //can have multiple allergies, so will have to make an array of all of them. this is temp
+			    .done(function(res) {
+				console.log("Do something here on success"); //temp
+				vm.recipes = res.data;
+			    });
+			    .fail(function(err) {
+				console.log(err);
+			    });
                     });
                 }
             });
